3479010d91985edf778cc28e5db1f878e1c2a77e
Thitiphat <thanakorn.wongk@mail.kmutt.ac.th>
2023-05-09 12:19:56 +0700

    first commit

---
+++ src/bankingapplication1/BankingApplication1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bankingapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class BankingApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int option = 0;
+        Scanner scan = new Scanner(System.in);
+        while (option != 6) {
+            System.out.println("Main Menu");
+            System.out.println("1. Display All Account");
+            System.out.println("2. Open New Account");
+            System.out.println("3. Close Existing Account");
+            System.out.println("4. Deposit");
+            System.out.println("5. Withdraw");
+            System.out.println("6. Exist");
+           
+            
+        }
+    }
+    
+}
